c7b18eeb6d2beba552b2c4da5a7cd9717646c275
Buckimus <ryan42buck@gmail.com>
2022-06-21 14:42:15 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -207,18 +207,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  // activate: function(event) {
-  //   console.log("activate", this);
-  // },
-  // deactivate: function(event) {
-  //   console.log("deactivate", this);
-  // },
-  // over: function(event) {
-  //   console.log("over", event.target);
-  // },
-  // out: function(event) {
-  //   console.log("out", event.target);
-  // },
+  activate: function(event,ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag")
+  },
+  deactivate: function(event,ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
+  },
   update: function(event) {
 
     var tempArr = [];
@@ -254,10 +256,10 @@ $("#trash").droppable({
     console.log("drop");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass(".bottom-trash-active")
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   }
 });
 
@@ -268,3 +270,8 @@ $("#modalDueDate").datepicker({
   }
 });
 
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
